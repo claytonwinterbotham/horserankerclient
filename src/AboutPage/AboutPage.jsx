7415de5aff7633f0bea9c5fd15e02f23aa52148d
Clayton Winterbotham <clayton@Claytons-MacBook-Air.local>
2018-04-18 19:05:45 -0700

    about page formatting

--- src/AboutPage/AboutPage.jsx
+++ src/AboutPage/AboutPage.jsx
@@ -19,8 +19,10 @@ class AboutPage extends React.Component {
                 <div className="row about-text">
                     <div className="col-8 col-lg-offset-2">
                         <h5>Horse Ranker is an Application that helps Horseplayers and Horse Race Handicappers
-                        compare and contrast the strengths and weaknesses of each horse in a Race. The Horse Ranker Application is currently in Beta development, 
-                        and future plans will allow Users to compare Trainers and Jockeys and Owners in terms of the characteristic rankings of horses which they Win races with.</h5>
+                            compare and contrast the strengths and weaknesses of each horse in a Race. Horse Ranker 
+                            provides Horseplayers and Handicappers with a quick Visual Comparison of 
+                            who the Top Ranked Horses are in each race, and which ones to watch out for when trying
+                            to predict the outcomes of races.</h5>
 
                         <h5>Users can select a specific Race at a Specific Racetrack and view a list of the horses in the race.
                         Then, they can see the Rankings of each horse on a number of Ratings and Variables
@@ -40,7 +42,7 @@ class AboutPage extends React.Component {
                             <li>Horses are also ranked in terms of how fast they ran in their most recent Three Races.</li>
 
                             <li>Horses are also ranked in terms of the Class or Level of Competition at which they have been 
-                            most successful in their past history of races.</li>
+                            most successfli in their past history of races.</li>
 
                             <li>Users can also see where each horse Ranks on Ratings of <strong>Probability of Winning</strong>, or <strong>Power Rankings.</strong></li>
 
@@ -48,10 +50,10 @@ class AboutPage extends React.Component {
                             their Finish Positions in their races, to see whether there are any Patterns evident which show
                             which kinds of races are won by horses with specific patterns of strengths or weaknesses.</li>
 
-                            <li>Horse Ranker provides Horseplayers and Handicappers with a quick Visual Comparison of 
-                            who the Top Ranked Horses are in each race, and which ones to watch out for when trying
-                            to predict the outcomes of races.</li>
                         </ul>
+                        <hr/>
+                        <h5>The Horse Ranker Application is currently in Beta development, and future plans will allow Users to compare
+                        Trainers and Jockeys and Owners in terms of the characteristic rankings of horses which they Win races with.</h5>
                 </div>        
               </div>  
             </div>
