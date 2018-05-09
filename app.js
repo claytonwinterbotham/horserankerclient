5d619082ab2c9e7413982d27a97ac1ac396b6cac
Clayton Winterbotham <claytonwinterbotham@Claytons-Mac-mini.local>
2018-05-08 21:48:10 -0700

    router fix 6

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app =  express();
 app.use(express.static(__dirname));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'https://horseranker.com/about.html')
+    res.sendFile(path.resolve(__dirname, 'https://horseranker.com/about.html'))
 });
 
 app.listen(port);
